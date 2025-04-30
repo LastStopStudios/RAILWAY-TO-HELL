f42d913246f88cf6e5cb6466ac5674c051ac13e5
Victor <victor.gonza88@gmail.com>
2025-04-30 09:32:03 +0200

    Map V2

--- Railway to hell/src/Mapa.cpp
+++ Railway to hell/src/Mapa.cpp
@@ -46,6 +46,7 @@ bool Mapa::Update(float dt) {
 
 	if (Engine::GetInstance().input.get()->GetKey(SDL_SCANCODE_M) == KEY_DOWN){
 		Mostrar = !Mostrar;
+		LoadMap();
 		if(!Mostrar){
 			Engine::GetInstance().scene->DialogoOff(); // Return control to all entities
 		}
@@ -80,18 +81,57 @@ bool Mapa::PostUpdate()
 
 bool Mapa::CleanUp()
 {
+	if (fondo != nullptr) {
+		Engine::GetInstance().textures->UnLoad(fondo);//Unload text background
+		fondo = nullptr;
+	}
+	if (mapa != nullptr) {
+		Engine::GetInstance().textures->UnLoad(mapa);//Unload text background
+		mapa = nullptr;
+	}
+	if (pj != nullptr) {
+		Engine::GetInstance().textures->UnLoad(pj);//Unload text background
+		mapa = nullptr;
+	}
+	if (cobertura != nullptr) {
+		Engine::GetInstance().textures->UnLoad(cobertura);//Unload text background
+		mapa = nullptr;
+	}
 	return true;
 }
 
 void Mapa::ShowMap() {
-	Engine::GetInstance().scene->DialogoOn(); // stop entities
-	int w, h;
-	Engine::GetInstance().window.get()->GetWindowSize(w, h);//Screen size
-	SDL_Rect dstRect = { 0, 0, w, h }; //Position and scale text background
+	SDL_Rect dstRect = { 0, 0, w, h }; //Position and scale of background and map texture
+	SDL_Rect dstRect2 = { posx,posy, 40, 40 }; //Position and scale character icon
 
+	SDL_RenderCopy(Engine::GetInstance().render->renderer, fondo, nullptr, &dstRect);
+	SDL_RenderCopy(Engine::GetInstance().render->renderer, mapa, nullptr, &dstRect);
+	SDL_RenderCopy(Engine::GetInstance().render->renderer, pj, nullptr, &dstRect2);
+}
+
+void Mapa::LoadMap(){
+	Engine::GetInstance().scene->DialogoOn(); // stop entities
+
+	//Scene on screen
 	int currentLvl = Engine::GetInstance().sceneLoader->GetCurrentLevel(); //bring out the current scene
 	scene = (currentLvl == 1) ? "scene" : "scene" + std::to_string(currentLvl);//pass the scene from where to get the dialogues
 
-	fondo = Engine::GetInstance().textures->Load("Assets/Textures/background/MapBackground.png "); //Load texture for text background
-	SDL_RenderCopy(Engine::GetInstance().render->renderer, fondo, nullptr, &dstRect);
+	//Map properties
+	posx = 200;//player position X
+	posy = 200;//player positon Y
+	Engine::GetInstance().window.get()->GetWindowSize(w, h);//Screen size
+
+	for (const auto& mapardo : mapardo) { // Iterate through all scenes
+		if (mapardo.escena == scene) { // Check where the player needs to go
+			mapaC = mapardo.mapardo; // map to load
+		}
+	}
+	
+	
+	
+
+	//Textures to load
+	fondo = Engine::GetInstance().textures->Load("Assets/Textures/mapa/MapBackground.png "); //Load texture for map background
+	mapa = Engine::GetInstance().textures->Load(mapaC.c_str()); //Load texture for map
+	pj = Engine::GetInstance().textures->Load("Assets/Textures/mapa/pj2.png "); //Load texture for character
 }
--- Railway to hell/src/Mapa.h
+++ Railway to hell/src/Mapa.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 
+
 struct SDL_Texture;
 
 class Mapa : public Module
@@ -32,15 +33,41 @@ public:
 	// Called before quitting
 	bool CleanUp();
 
+	struct MapaQueCargar {
+		std::string escena;
+		std::string mapardo;
+	};
+	//list of scenes with their id
+	std::vector<MapaQueCargar> mapardo = {
+		//sensor id list with info: scene to load, player's x, player's y, fade in and fade out, boss camera
+		{"scene","Assets/Textures/mapa/Mapa.png"},//map for scene
+		{"scene1","Assets/Textures/mapa/Mapa.png"},//map for scene 1
+		{"scene2","Assets/Textures/mapa/Mapa.png"},//map for scene 2
+		{"scene3","Assets/Textures/mapa/Mapa.png"},//map for scene 3
+		{"scene4","Assets/Textures/mapa/Mapa.png"},//map for scene 4
+		{"scene5","Assets/Textures/mapa/Mapa.png"},//map for scene 5
+	};
+
 private:
 	// Private methods
 	void ShowMap();
+	void LoadMap();
 
 public:
 	bool Mostrar;
 
 private:
 	SDL_Texture* fondo = nullptr;//background texture
-	std::string scene = "";
+	SDL_Texture* mapa = nullptr;//Map texture
+	SDL_Texture* pj = nullptr;//Map texture
+	SDL_Texture* cobertura = nullptr;//Black texture
+	std::string scene = "";//what scene is now loading
+	std::string mapaC = "";//map to show
+	std::string pjposition = "";//what scene is now loading
+	int posx, posy;// playerposition
+	int w, h;// screen size
+
+
+
 };
 
