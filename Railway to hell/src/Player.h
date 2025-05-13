e2ab461a0858710d2ecea0ddb5b740eba5184a7a
Victor <victor.gonza88@gmail.com>
2025-05-13 22:52:55 +0200

    enemy hit fix V2.0.1

--- Railway to hell/src/Player.h
+++ Railway to hell/src/Player.h
@@ -269,12 +269,6 @@ private:
 
 	//colliding with enemies
 	std::chrono::time_point<std::chrono::steady_clock> lastHitTime;
-	bool tocado = false;//controls cooldown
-	bool first = true;
-	float hitCooldown = 3000.0f;// 3 seconds
-	float initialHitCooldown = 0.0f; // Temporizador después del primer golpe
-	float continuousHitTimer = 0.0f; // Temporizador entre daños continuos
-
 	bool isTouchingEnemy = false;
 	bool wasTouchingEnemy = false;
 	float damageCooldown = 0.0f;
